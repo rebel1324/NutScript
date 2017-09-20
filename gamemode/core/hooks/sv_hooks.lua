09b79110b5cd442ac7950a2f43cbb69dbb2b7b0c
Thadah Denyse <thadahdenyse@gmail.com>
2017-09-20 18:17:46 +0200

    Made a mistake

--- gamemode/core/hooks/sv_hooks.lua
+++ gamemode/core/hooks/sv_hooks.lua
@@ -588,15 +588,17 @@ end
 function GM:PlayerCanHearPlayersVoice(listener, speaker)
 	local allowVoice = nut.config.get("allowVoice")
 	local distanceVoice = nut.config.get("3dVoice")
-	if (allowVoice and distanceVoice) then
-		local listener_pos = listener:GetPos()
-		local speaker_pos = speaker:GetPos()
-		local voice_dis = math.Distance(speaker_pos.x, speaker_pos.y, listener_pos.x, listener_pos.y)
-		if voice_dis > nut.config.get("voiceDistance") then
-			allowVoice = false
-		end
-	else
+	if (allowVoice) then
 		allowVoice = true
+		if (distanceVoice) then
+			local listener_pos = listener:GetPos()
+			local speaker_pos = speaker:GetPos()
+			local voice_dis = math.Distance(speaker_pos.x, speaker_pos.y, listener_pos.x, listener_pos.y)
+			if voice_dis > nut.config.get("voiceDistance") then
+				allowVoice = false
+			end
+	else
+		allowVoice = false
 	end
 	return allowVoice, allowVoice
 end
