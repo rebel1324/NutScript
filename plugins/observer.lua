bbfb869fdd85674a1df22d4272890d02bc71484d
George Butler <georgeleobutler@gmail.com>
2017-11-01 16:49:27 -0700

    Added the ability to toggle Admin ESP lines. Some users complained that these lines confused them.

--- plugins/observer.lua
+++ plugins/observer.lua
@@ -6,12 +6,13 @@ if (CLIENT) then
 	-- Create a setting to see if the player will teleport back after noclipping.
 	NUT_CVAR_OBSTPBACK = CreateClientConVar("nut_obstpback", 0, true, true)
 	NUT_CVAR_ADMINESP = CreateClientConVar("nut_obsesp", 1, true, true)
+	NUT_CVAR_ADMINESPLINES = CreateClientConVar("nut_obsesplines", 1, true, true)
 
 	local client, sx, sy, scrPos, marginx, marginy, x, y, teamColor, distance, factor, size, alpha
 	local dimDistance = 1024
 	function PLUGIN:HUDPaint()
 		client = LocalPlayer()
-		
+
 		if (client:IsAdmin() and client:GetMoveType() == MOVETYPE_NOCLIP and !client:InVehicle() and NUT_CVAR_ADMINESP:GetBool()) then
 			sx, sy = surface.ScreenWidth(), surface.ScreenHeight()
 
@@ -28,7 +29,9 @@ if (CLIENT) then
 				alpha = math.Clamp(255*factor, 80, 255)
 
 				surface.SetDrawColor(teamColor.r, teamColor.g, teamColor.b, alpha)
-				surface.DrawLine(sx * 0.5, sy * 0.5, x, y)
+				if (NUT_CVAR_ADMINESPLINES:GetBool()) then
+					surface.DrawLine(sx * 0.5, sy * 0.5, x, y)
+				end
 				surface.DrawRect(x - size/2, y - size/2, size, size)
 
 				nut.util.drawText(v:Name(), x, y - size, ColorAlpha(teamColor, alpha), TEXT_ALIGN_CENTER, TEXT_ALIGN_CENTER, nil, alpha)
@@ -45,7 +48,15 @@ if (CLIENT) then
 					RunConsoleCommand("nut_obsesp", "0")
 				end
 			end, NUT_CVAR_ADMINESP:GetBool())
-		
+
+			local buttonESPLines = menu:addCheck(L"toggleESPLines", function(panel, state)
+				if (state) then
+					RunConsoleCommand("nut_obsesplines", "1")
+				else
+					RunConsoleCommand("nut_obsesplines", "0")
+				end
+			end, NUT_CVAR_ADMINESPLINES:GetBool())
+
 			local buttonTP = menu:addCheck(L"toggleObserverTP", function(panel, state)
 				if (state) then
 					RunConsoleCommand("nut_obstpback", "1")
