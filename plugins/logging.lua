bf4e943fdde0c3b72958ccd53544c8e3de4c9804
Sam Nyashev <ll2.lol@yandex.ru>
2017-12-01 00:37:56 +0300

    Update logging.lua

--- plugins/logging.lua
+++ plugins/logging.lua
@@ -1,53 +1,52 @@
+local PLUGIN = PLUGIN
 PLUGIN.name = "Logging"
 PLUGIN.author = "Black Tea"
 PLUGIN.desc = "You can modfiy the logging text/lists on this plugin."
  
 if (SERVER) then
+	local L, type = Format, type
+	
 	nut.log.addType("chat", function(client, ...)
 		local arg = {...}
-		return (Format("[%s] %s: %s", arg[1], client:Name(), arg[2]))
+		return (L("[%s] %s: %s", arg[1], client:Name(), arg[2]))
 	end)
 	nut.log.addType("command", function(client, ...)
 		local arg = {...}
-		return (Format("%s used command '%s'", client:Name(), arg[1]))
+		return (L("%s used command '%s'", client:Name(), arg[1]))
 	end)
 	nut.log.addType("charLoad", function(client, ...)
 		local arg = {...}
-		return (Format("%s loaded the character #%s(%s)", client:Name(), arg[1], arg[2]))
+		return (L("%s loaded the character #%s(%s)", client:Name(), arg[1], arg[2]))
 	end)
 	nut.log.addType("charDelete", function(client, ...)
 		local arg = {...}
-		return (Format("%s(%s) deleted character (%s)", client:steamName(), client:SteamID(), arg[1]))
+		return (L("%s(%s) deleted character (%s)", client:steamName(), client:SteamID(), arg[1]))
 	end)
 	nut.log.addType("itemUse", function(client, ...)
 		local arg = {...}
 		local item = arg[2]
-		return (Format("%s tried '%s' to item '%s'(#%s)", client:Name(), arg[1], item.name, item.id))
+		return (L("%s tried '%s' to item '%s'(#%s)", client:Name(), arg[1], item.name, item.id))
 	end)
 	nut.log.addType("shipment", function(client, ...)
 		local arg = {...}
-		return (Format("%s took '%s' from the shipment", client:Name(), arg[1]))
+		return (L("%s took '%s' from the shipment", client:Name(), arg[1]))
 	end)
 	nut.log.addType("shipmentO", function(client, ...)
 		local arg = {...}
-		return (Format("%s ordered a shipment", client:Name()))
+		return (L("%s ordered a shipment", client:Name()))
 	end)
 	nut.log.addType("buy", function(client, ...)
 		local arg = {...}
-		return (Format("%s purchased '%s' from the NPC", client:Name(), arg[1]))
+		return (L("%s purchased '%s' from the NPC", client:Name(), arg[1]))
 	end)
 	nut.log.addType("buydoor", function(client, ...)
 		local arg = {...}
-		return (Format("%s purchased the door", client:Name()))
+		return (L("%s purchased the door", client:Name()))
 	end)
-
-	local L = Format
 
 	function PLUGIN:CharacterLoaded(id)
 		local character = nut.char.loaded[id]
 		local client = character:getPlayer()
-
-		--nut.log.add(client:steamName().." ("..client:SteamID()..") loaded character #"..id.." ("..character:getName()..")")
 		nut.log.add(client, "charLoad", id, character:getName())
 	end
 
@@ -66,13 +65,6 @@ if (SERVER) then
 
 	function PLUGIN:OnCharTradeVendor(client, vendor, x, y, invID, price, isSell)
 	end
-
-	local logInteractions = {
-		["drop"] = true,
-		["take"] = true,
-		["equip"] = true,
-		["unequip"] = true,
-	}
 
 	function PLUGIN:OnPlayerInteractItem(client, action, item)
 		if (type(item) == "Entity") then
