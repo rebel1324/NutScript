bdc3ee9284ba3c95dd71c547dbeab4350ef0fea1
Kyu Yeon Lee <rebel1324@gmail.com>
2018-04-10 10:41:11 +0900

    Added 3D2D Blur Library as plugin.

---
+++ plugins/blur3d2d.lua
@@ -0,0 +1,85 @@
+PLUGIN.name = "3D2D Blur Library"
+PLUGIN.author = "Black Tea"
+PLUGIN.desc = "Plugin for Developers."
+
+nut.blur3d2d = nut.blur3d2d or {}
+nut.blur3d2d.list = {}
+
+-- for easy managements
+function nut.blur3d2d.add(id, pos, ang, scale, callback)
+    if (id and pos and ang and scale and callback) then
+        nut.blur3d2d.list[id] = {
+            pos = pos, 
+            ang = ang,
+            scale = scale,
+            callback = callback,
+            draw = true,
+        }
+    end
+end
+
+-- remove the screen from the rendering list completely.
+function nut.blur3d2d.remove(id)
+    if (id and nut.blur3d2d.list[id]) then
+        nut.blur3d2d.list[id] = nil
+    end
+end
+
+-- pause drawing the screen. does not removes the info from the table.
+function nut.blur3d2d.pause(id)
+    if (id and nut.blur3d2d.list[id]) then
+        nut.blur3d2d.list[id].draw = false
+    end
+end
+
+-- start drawing the screen again.
+function nut.blur3d2d.resume(id)
+    if (id and nut.blur3d2d.list[id]) then
+        nut.blur3d2d.list[id].draw = true
+    end
+end
+
+hook.Add("PostDrawTranslucentRenderables", "blur", function()
+    render.ClearStencil()
+    render.SetStencilEnable(true)
+    
+    -- for avoid conflict with poorly written stencil badbois
+    render.SetStencilWriteMask(99)
+    render.SetStencilTestMask(99)
+    render.SetStencilFailOperation(STENCILOPERATION_KEEP)
+    render.SetStencilZFailOperation(STENCILOPERATION_KEEP)
+    render.SetStencilPassOperation(STENCILOPERATION_REPLACE)
+    render.SetStencilCompareFunction(STENCILCOMPARISONFUNCTION_ALWAYS)
+    render.SetStencilReferenceValue(1)
+    
+    -- I had to change this because I don't want to make draw post process compute run by O(n) 
+    SUPPRESS_FROM_STENCIL = true
+        for _, data in pairs(nut.blur3d2d.list) do
+            if (data.draw == false) then continue end
+
+            cam.Start3D2D(data.pos, data.ang, data.scale)
+                data.callback()
+            cam.End3D2D()
+        end     
+
+        render.SetStencilReferenceValue(2)
+        render.SetStencilCompareFunction( STENCILCOMPARISONFUNCTION_EQUAL )
+        render.SetStencilPassOperation( STENCILOPERATION_REPLACE )
+        render.SetStencilReferenceValue(1)
+        cam.Start2D()
+            nut.util.drawBlurAt(0, 0, ScrW(), ScrH())
+        cam.End2D()
+        render.SetStencilEnable( false )
+
+        for _, data in pairs(nut.blur3d2d.list) do
+            if (data.draw == false) then continue end
+
+            cam.Start3D2D(data.pos, data.ang, data.scale)
+                data.callback(true)
+            cam.End3D2D()
+        end
+    SUPPRESS_FROM_STENCIL = nil
+end)
+
+--[[
+]]--
