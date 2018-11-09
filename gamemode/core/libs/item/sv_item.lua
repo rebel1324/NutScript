922ebf161ca222689578385a552f25b4cef17a6a
Brian Hang <hangwithbrian@gmail.com>
2018-11-09 09:22:20 -0800

    Fix item metatable not working with autoreload

---
+++ gamemode/core/libs/item/sv_item.lua
@@ -0,0 +1,112 @@
+function nut.item.instance(index, uniqueID, itemData, x, y, callback)
+	local itemTable = nut.item.list[uniqueID]
+	if (not itemTable) then
+		error("Attempt to instantiate invalid item "..tostring(uniqueID))
+	end
+
+	if (not istable(itemData)) then
+		itemData = {}
+	end
+
+	-- Legacy support for x, y data: have the x, y data save to the correct
+	-- x, y column instead of the data column
+	if (isnumber(itemData.x)) then
+		x = itemData.x
+		itemData.x = nil
+	end
+	if (isnumber(itemData.y)) then
+		y = itemData.y
+		itemData.y = nil
+	end
+
+	local function onItemCreated(data, itemID)
+		local item = nut.item.new(uniqueID, itemID)
+
+		if (item) then
+			item.data = itemData
+			item.invID = index
+
+			-- Legacy support for x, y data: add it back to the data for use
+			item.data.x = x
+			item.data.y = y
+
+			if (callback) then
+				callback(item)
+			end
+
+			item:onInstanced(index, x, y, item)
+		end
+	end
+
+	if (MYSQLOO_PREPARED) then
+		nut.db.preparedCall(
+			"itemInstance", onItemCreated, index, uniqueID, itemData, x, y
+		)
+	else
+		nut.db.insertTable({
+			_invID = index,
+			_uniqueID = uniqueID,
+			_data = itemData,
+			_x = x,
+			_y = y
+		}, onItemCreated, "items")
+	end
+end
+
+function nut.item.deleteByID(id)
+	if (nut.item.instances[id]) then
+		nut.item.instances[id]:delete()
+	else
+		nut.db.delete("items", "_itemID = "..id)
+	end
+end
+
+function nut.item.loadItemByID(itemIndex, recipientFilter)
+	local range
+	if (type(itemIndex) == "table") then
+		range = "("..table.concat(itemIndex, ", ")..")"
+	elseif (type(itemIndex) == "number") then
+		range = "(".. itemIndex ..")"
+	else
+		return
+	end
+
+	nut.db.query("SELECT _itemID, _uniqueID, _data, _x, _y FROM nut_items WHERE _itemID IN "..range, function(data)
+		if (data) then
+			for k, v in ipairs(data) do
+				local itemID = tonumber(v._itemID)
+				local data = util.JSONToTable(v._data or "[]")
+				local uniqueID = v._uniqueID
+				local itemTable = nut.item.list[uniqueID]
+
+				if (itemTable and itemID) then
+					local item = nut.item.new(uniqueID, itemID)
+
+					item.invID = 0
+					item.data = data or {}
+
+					-- Legacy support for x, y data
+					item.data.x = tonumber(v._x)
+					item.data.y = tonumber(v._y)
+
+					item:onRestored()
+				end
+			end
+		end
+	end)
+end
+
+-- Instances and spawns a given item type.
+function nut.item.spawn(uniqueID, position, callback, angles, data)
+	nut.item.instance(0, uniqueID, data or {}, 1, 1, function(item)
+		if (item.isStackable) then
+			item:setQuantity(item:getMaxQuantity())
+		end
+
+		local entity = item:spawn(position, angles)
+
+		if (callback) then
+			callback(item, entity)
+		end
+	end)
+end
---
+++ gamemode/core/libs/item/sv_networking.lua
@@ -0,0 +1,44 @@
+util.AddNetworkString("nutCharacterInvList")
+util.AddNetworkString("nutItemDelete")
+util.AddNetworkString("nutItemInstance")
+
+netstream.Hook("invAct", function(client, action, item, invID, data)
+	local character = client:getChar()
+	if (!character) then
+		return
+	end
+
+	-- Refine item into an instance
+	local entity
+	if (type(item) == "Entity") then
+		if (not IsValid(item)) then
+			return
+		end
+		if (item:GetPos():Distance(client:GetPos()) > 96) then
+			return
+		end
+		if (not item.nutItemID) then
+			return
+		end
+		entity = item
+		item = nut.item.instances[item.nutItemID]
+	else
+		item = nut.item.instances[item]
+	end
+	if (not item) then
+		return
+	end
+	-- Permission check with inventory. Or, if no inventory exists,
+	-- the player has no way of accessing the item.
+	local inventory = nut.inventory.instances[item.invID]
+	local context = {
+		client = client, item = item, entity = entity, action = action
+	}
+	if (
+		inventory and not inventory:canAccess("item", context)
+	) then
+		return
+	end
+
+	item:interact(action, client, entity, data)
+end)
