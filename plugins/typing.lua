ce62fd27df8ad67c0e5df134a93eadc11b2cf668
Brian Hang <hangwithbrian@gmail.com>
2018-12-09 21:37:05 -0800

    Fix typing plugin not working

--- plugins/typing.lua
+++ plugins/typing.lua
@@ -8,46 +8,59 @@ if (CLIENT) then
 	local TYPE_COLOR = Color(250, 250, 250)
 
 	function PLUGIN:StartChat()
-		netstream.Start("typeStatus", 1)
+		net.Start("nutTypeStatus")
+			net.WriteBool(true)
+		net.SendToServer()
 	end
 
 	function PLUGIN:FinishChat()
-		netstream.Start("typeStatus")
+		net.Start("nutTypeStatus")
+			net.WriteBool(false)
+		net.SendToServer()
 	end
 
 	local data = {}
+	local offset1, offset2, offset3, alpha, y
 
 	function PLUGIN:HUDPaint()
 		local ourPos = LocalPlayer():GetPos()
 		local localPlayer = LocalPlayer()
+		local time = RealTime() * 5
 
 		data.start = localPlayer:EyePos()
+		data.filter = localPlayer
 
 		for k, v in ipairs(player.GetAll()) do
-			if (v != localPlayer and v:getNetVar("typing") and v:GetMoveType() == MOVETYPE_WALK) then
+			if (
+				v ~= localPlayer and
+				v:getNetVar("typing") and
+				v:GetMoveType() == MOVETYPE_WALK
+			) then
 				data.endpos = v:EyePos()
+				if (util.TraceLine(data).Entity ~= v) then continue end
+				local position = v:GetPos()
+				alpha = (1 - (ourPos:DistToSqr(position) / 65536)) * 255
+				if (alpha <= 0) then continue end
 
-				if (util.TraceLine(data).Entity == v) then
-					local position = v:GetPos()
-					local alpha = (1 - (ourPos:DistToSqr(position) / 65536)) * 255
+				local screen = (
+					position +
+					(v:Crouching() and TYPE_OFFSET_CROUCHED or TYPE_OFFSET)
+				):ToScreen()
+				offset1 = math.sin(time + 2) * alpha
+				offset2 = math.sin(time + 1) * alpha
+				offset3 = math.sin(time) * alpha
+				y = screen.y - 20
 
-					if (alpha > 0) then
-						local screen = (position + (v:Crouching() and TYPE_OFFSET_CROUCHED or TYPE_OFFSET)):ToScreen()
-
-						nut.util.drawText("(Typing)", screen.x, screen.y - 18, ColorAlpha(TYPE_COLOR, alpha), 1, 1, "nutChatFontItalics", alpha)
-					end
-				end
+				nut.util.drawText("•", screen.x - 8, y, ColorAlpha(TYPE_COLOR, offset1), 1, 1, "nutChatFont", offset1)
+				nut.util.drawText("•", screen.x, y, ColorAlpha(TYPE_COLOR, offset2), 1, 1, "nutChatFont", offset2)
+				nut.util.drawText("•", screen.x + 8, y, ColorAlpha(TYPE_COLOR, offset3), 1, 1, "nutChatFont", offset3)
 			end
 		end
 	end
 else
-	netstream.Hook("typeStatus", function(client, state)
-		if (state) then
-			state = true
-		else
-			state = nil
-		end
+	util.AddNetworkString("TypeStatus")
 
-		client:setNetVar("typing", state)
+	net.Receive("nutTypeStatus", function(_, client)
+		client:setNetVar("typing", net.ReadBool())
 	end)
 end
