0908358da2a79ec543816dc0424927db32d16e36
Rubberbandits <Rubberbandits@users.noreply.github.com>
2019-09-08 02:01:34 -0700

    Update sh_scash_abuse.lua

--- plugins/sh_scash_abuse.lua
+++ plugins/sh_scash_abuse.lua
@@ -1,8 +1,8 @@
 /* Due to the amount of servers that have been absolutely braindead in their configuration, allowing assholes like me to write a nice little script to infinitely spawn money, I thought it's high time people used their brains. */
 
-PLUGIN.name = "Crosshair"
-PLUGIN.author = "Black Tea"
-PLUGIN.desc = "A Crosshair."
+PLUGIN.name = "Spawning money Anti-Abuse"
+PLUGIN.author = "Rusty"
+PLUGIN.desc = "Prevent people from using ye-olde classic money maker."
 
 function PLUGIN:CanDeleteChar(ply, char)
   if char:getMoney() < nut.config.get("defMoney", 0) then
