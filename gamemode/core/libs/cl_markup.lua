cb96946c9ca7ac8c5c419818834c26526790d2e6
Zoephix <heijmanszep2@gmail.com>
2021-02-04 20:07:56 +0100

    Update gamemode/core/libs/cl_markup.lua
    Co-authored-by: TovarischPootis <54110479+TovarischPootis@users.noreply.github.com>

--- gamemode/core/libs/cl_markup.lua
+++ gamemode/core/libs/cl_markup.lua
@@ -159,7 +159,7 @@ local function CheckTextOrTag(p)
 		
 		local text_block = {}
 		text_block.text = p 
-		text_block.colour = colour_stack[ table.Count(colour_stack) ]
+		text_block.colour = colour_stack[#colour_stack ]
 		text_block.font = font_stack[ table.Count(font_stack) ]
 		table.insert(blocks, text_block)
 		
